88713a6bf1f27f2a362101880c5090a0d56f7971
Tim Robinson <tim.g.robinson@gmail.com>
2017-04-27 12:57:42 +0100

    Cache dependencies in the Docker image

--- docker/build.sh
+++ docker/build.sh
@@ -4,6 +4,7 @@ IFS=$'\n\t'
 git clone /host /build
 (
     cd /build
+    rsync --times --links --ignore-existing --recursive /host-bootstrap/3rdparty/ 3rdparty
     ./travis-setup.sh
     ./travis.sh
 )
