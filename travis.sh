fc917cc7d93013ebeb700ff7dab494dc34cf8fd8
Tim Robinson <tim.g.robinson@gmail.com>
2017-05-02 19:32:46 +0100

    Makefile

--- travis.sh
+++ travis.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
 IFS=$'\n\t'
-export PATH=$PATH:$(pwd)/3rdparty/binutils/bin:~/.cargo/bin
-(
-    cd src
-    make -C kernel
-    make -C apps/input
-    make -C apps/hello
-    make -C boot
-)
+PATH=$PATH:$(pwd)/3rdparty/binutils/bin:~/.cargo/bin make -C src
 python3 test.py qemu-system-x86_64 -no-kvm -display vnc=:1 -no-reboot -kernel src/kernel/kernel -initrd src/boot/initrd.tar
