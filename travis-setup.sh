394290fec8f69319f219c5b03f6e85816a1bd002
Tim Robinson <tim.g.robinson@gmail.com>
2017-05-29 21:20:22 +0100

    Latest QEMU on Travis Trusty

--- travis-setup.sh
+++ travis-setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -xeuo pipefail
 IFS=$'\n\t'
+export PATH=$PATH:$HOME/.cargo/bin:$(pwd)/3rdparty/bin
 source src/config.txt
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain $CONFIG_RUST_TOOLCHAIN
-PATH=$PATH:~/.cargo/bin
 which xargo || cargo install xargo
 rustup toolchain install $CONFIG_RUST_TOOLCHAIN
 rustup component add --toolchain=$CONFIG_RUST_TOOLCHAIN rust-src
-make -s -C 3rdparty binutils
+make -s -C 3rdparty tools
+x86_64-elf-ld --version
+qemu-system-x86_64 --version
 pip3 install -r requirements.txt
--- travis.sh
+++ travis.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -xeuo pipefail
 IFS=$'\n\t'
-PATH=$PATH:$(pwd)/3rdparty/binutils/bin:~/.cargo/bin make -C src
+export PATH=$PATH:$HOME/.cargo/bin:$(pwd)/3rdparty/bin
+make -C src
 python3 test.py qemu-system-x86_64 -no-kvm -display vnc=:1 -no-reboot -kernel src/kernel/kernel -initrd src/boot/initrd.tar
