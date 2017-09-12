0211cbdb70ee34327bfa9cac58afe4d891ae24af
Tim Robinson <tim.g.robinson@gmail.com>
2017-09-12 20:31:01 +0100

    Another user mode test

--- src/libgraphics/widget.rs
+++ src/libgraphics/widget.rs
@@ -19,7 +19,7 @@ fn ref_eq<T>(a: &T, b: &T) -> bool {
     a as *const T == b as *const T
 }
 
-impl<T: Widget> WidgetTree<T> {
+impl<T> WidgetTree<T> {
     pub fn new() -> Self {
         WidgetTree {
             paint_needed: true,
@@ -38,7 +38,7 @@ impl<T: Widget> WidgetTree<T> {
         let index_opt =
             self.by_zorder
                 .iter()
-                .position(|w| ref_eq::<T>(&*w, &*widget));
+                .position(|w| ref_eq::<T>(w.as_ref(), widget.as_ref()));
 
         if let Some(index) = index_opt {
             self.by_zorder.remove(index);
@@ -60,6 +60,24 @@ impl<T: Widget> WidgetTree<T> {
         self.paint_needed = true;
     }
 
+    pub fn get_paint_needed(&mut self) -> bool {
+        self.paint_needed
+    }
+
+    pub fn set_paint_needed(&mut self) {
+        self.paint_needed = true;
+    }
+
+    pub fn get_focus(&mut self) -> Option<&Arc<T>> {
+        self.focus.as_ref()
+    }
+
+    pub fn get_focus_mut(&mut self) -> Option<&mut Arc<T>> {
+        self.focus.as_mut()
+    }
+}
+
+impl<T: Widget> WidgetTree<T> {
     pub fn move_to(&mut self, widget: &Arc<T>, pos: Rect) -> Result<()> {
         widget.move_to(pos)?;
         self.paint_needed = true;
@@ -77,22 +95,39 @@ impl<T: Widget> WidgetTree<T> {
 
         self.paint_needed = false;
     }
-
-    pub fn get_paint_needed(&mut self) -> bool {
-        self.paint_needed
-    }
-
-    pub fn set_paint_needed(&mut self) {
-        self.paint_needed = true;
-    }
-
-    pub fn get_focus_mut(&mut self) -> Option<&mut Arc<T>> {
-        self.focus.as_mut()
-    }
 }
 
 #[cfg(feature = "test")]
 pub mod test {
+    use super::*;
+
+    pub trait PartialEqWith {
+        type Inner;
+        fn eq_with<F: FnOnce(&Self::Inner, &Self::Inner) -> bool>(&self, other: &Self, f: F) -> bool;
+    }
+
+    impl<T> PartialEqWith for Option<T> {
+        type Inner = T;
+
+        fn eq_with<F: FnOnce(&Self::Inner, &Self::Inner) -> bool>(&self, other: &Self, f: F) -> bool {
+            match (self.as_ref(), other.as_ref()) {
+                (Some(ref a), Some(ref b)) => f(a, b),
+                (None, None) => true,
+                _ => false
+            }
+        }
+    }
+
     test! {
+        fn add_changes_focus() {
+            let mut tree = WidgetTree::new();
+            assert_eq!(None, tree.get_focus());
+
+            let one = Arc::new(1);
+            tree.add(one.clone());
+            assert!(Some(one).eq_with(&tree.get_focus().cloned(), |a, b| {
+                ref_eq::<i32>(a.as_ref(), b.as_ref())
+            }));
+        }
     }
 }
