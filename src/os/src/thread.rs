9c631530f8575e9fa8a97354ed3bb4e742fd895a
Tim Robinson <tim.g.robinson@gmail.com>
2021-06-18 16:10:12 +0100

    Thread returns `impl Termination`

--- src/os/src/thread.rs
+++ src/os/src/thread.rs
@@ -1,10 +1,49 @@
 use crate::{OSHandle, Result};
 use alloc::boxed::Box;
 use syscall;
+use syscall::ErrNum;
 
-extern "C" fn thread_entry(context: usize) {
-    let b: Box<Box<dyn FnOnce() -> i32>> = unsafe { Box::from_raw(context as *mut _) };
-    let code = b();
+#[lang = "termination"]
+pub trait Termination {
+    fn report(self) -> i32;
+}
+
+impl Termination for () {
+    fn report(self) -> i32 {
+        Ok(()).report()
+    }
+}
+
+impl Termination for Result<()> {
+    fn report(self) -> i32 {
+        self.map(|()| 0).report()
+    }
+}
+
+impl Termination for i32 {
+    fn report(self) -> i32 {
+        -self
+    }
+}
+
+impl Termination for ErrNum {
+    fn report(self) -> i32 {
+        (self as i32).report()
+    }
+}
+
+impl Termination for Result<i32> {
+    fn report(self) -> i32 {
+        self.unwrap_or_else(|num| num.report())
+    }
+}
+
+extern "C" fn thread_entry<T>(context: usize)
+where
+    T: Termination,
+{
+    let b: Box<Box<dyn FnOnce() -> T>> = unsafe { Box::from_raw(context as *mut _) };
+    let code = b().report();
     Thread::exit(code)
 }
 
@@ -28,13 +67,20 @@ impl Thread {
         Thread(handle)
     }
 
-    fn spawn_inner(b: Box<Box<dyn FnOnce() -> i32>>) -> Self {
+    fn spawn_inner<T>(b: Box<Box<dyn FnOnce() -> T>>) -> Self
+    where
+        T: Termination,
+    {
         let context_ptr = Box::into_raw(b);
-        let handle = syscall::spawn_thread(thread_entry, context_ptr as usize);
+        let handle = syscall::spawn_thread(thread_entry::<T>, context_ptr as usize);
         Self(OSHandle::from_raw(handle))
     }
 
-    pub fn spawn<T: 'static + FnOnce() -> i32 + Send>(entry: T) -> Self {
+    pub fn spawn<F, T>(entry: F) -> Self
+    where
+        F: FnOnce() -> T + Send + 'static,
+        T: Termination,
+    {
         Self::spawn_inner(Box::new(Box::new(entry)))
     }
 
